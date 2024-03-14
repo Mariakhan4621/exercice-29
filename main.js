7db84a6c9684a20545c2ca213a4f430c99d25031
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-14 20:22:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// you really like bananas
+var favourite_fruits = ["mango", "grapes", "water melon", "cherry", "guava"];
+if (favourite_fruits.includes("mango")) {
+    console.log("mango");
+}
+if (favourite_fruits.includes("grapes")) {
+    console.log("grapes");
+}
+if (favourite_fruits.includes("water melon")) {
+    console.log("water melon");
+}
+if (favourite_fruits.includes("cherry")) {
+    console.log("you really like bananas");
+}
+if (favourite_fruits.includes("guava")) {
+    console.log("you really like bananas");
+}
+if (favourite_fruits.includes("cherry")) {
+    console.log("you really like bananas");
+}
+if (favourite_fruits.includes("guava")) {
+    console.log("you really like bananas");
+}
---
+++ main.ts
@@ -0,0 +1,31 @@
+// you really like bananas
+
+let favourite_fruits : string [] = ["mango","grapes","water melon","cherry","guava"]
+
+if (favourite_fruits.includes("mango")) {
+    console.log("mango")
+}
+
+if (favourite_fruits.includes("grapes")) {
+    console.log("grapes")
+}
+
+if (favourite_fruits.includes("water melon")) {
+    console.log("water melon")
+}
+
+if (favourite_fruits.includes("cherry")) {
+    console.log("you really like bananas")
+}
+
+if (favourite_fruits.includes("guava")) {
+    console.log("you really like bananas")
+}
+
+if (favourite_fruits.includes("cherry")) {
+    console.log("you really like bananas")
+}
+
+if (favourite_fruits.includes("guava")) {
+    console.log("you really like bananas")
+}
